f67645680da0173858ac3ec2eb181e1a67898faa
vhCabral <victor5717oficial@gmail.com>
2025-10-06 18:58:59 -0300

    project created

---
+++ src/test/java/br/com/victorcabral/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorcabral.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
